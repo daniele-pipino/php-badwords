d77961084fbbbca040feffb214f7104278235666
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-21 13:34:00 +0200

    stampa paragrafo e relativa lunghezza

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-badwords</title>
+</head>
+<body>
+    <?php
+    // traccia esercizio 
+    /*
+    Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+     */ 
+
+
+    //  creazione variabile con paragrafo di testo
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, numquam. Alias in quasi voluptate, perferendis facilis praesentium nemo sit odit aliquam iure dolorem numquam illo ducimus a at laboriosam possimus!';
+
+    ?>
+    <!-- stampa variabile -->
+    <p>
+        <?php echo strlen($text); 
+        echo $text; ?>
+    </p>
+</body>
+</html>
